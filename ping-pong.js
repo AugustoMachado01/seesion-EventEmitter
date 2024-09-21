e74c36c69ba2f6fcaa9795e9f11cc96f0435d340
AugustoMachado01 <augustomachadochieno01@gmail.com>
2024-09-21 09:06:11 +0100

    Emitter error

--- ping-pong.js
+++ ping-pong.js
@@ -1,6 +1,6 @@
 const { EventEmitter } = require("events");
 
-((async) => {
+pingpongServer = () => {
   {
     const pingpong = new EventEmitter();
 
@@ -20,25 +20,63 @@ const { EventEmitter } = require("events");
 
     pingpong.emit("ping");
   }
+};
+const delay = (time) => {
+  return new Promise((resolve) => setTimeout(resolve, time));
+};
 
-  const delay = (time) => {
-    new Promise((resolve) => setTimeout(resolve, time));
+const pingpongP = () => {
+  const pingpong = new EventEmitter();
+
+  pingpong
+    .on("ping", async () => {
+      console.log("ping");
+      await delay(500);
+      pingpong.emit("pong");
+    })
+    .on("pong", async () => {
+      console.log("pong");
+      pingpong.emit("ping");
+    });
+
+  pingpong.emit("ping");
+};
+
+// pingpongP();
+
+const error = async () => {
+  const pingpong = new EventEmitter({
+    captureRejections: true,
+  });
+
+  pingpong
+    .on("ping", async () => {
+      console.log("ping");
+      await delay(500);
+      pingpong.emit("pong");
+    })
+    .on("pong", async () => {
+      console.log("pong");
+      await delay(500);
+      pingpong.emit("ping");
+    })
+    .on("error", (error) => {
+      console.error("eu captureu o erro  via on", error.message);
+    })
+    .on("forceExplodeAsync", (error) => Promise.reject(error));
+
+  // node 14
+  //prioritario
+  pingpong[Symbol.for("nodejs.rejection")] = (error) => {
+    console.error("eu capturei o erro  via propriedade", error.message);
   };
 
-  const pingpongP = () => {
-    const pingpong = new EventEmitter();
+  pingpong.emit("ping");
+  await delay(2000);
+  pingpong.emit("error", Error("deu ruin depois de 2s"));
 
-    pingpong
-      .on("ping", async () => {
-        console.log("ping");
-        delay(500);
-        pingpong.emit("pong");
-      })
-      .on("pong", async () => {
-        console.log("pong");
-        pingpong.emit("ping");
-      });
+  await delay(1000);
+  pingpong.emit("forceExplodeAsync", Error("deu ruin depois de 3s"));
+};
 
-    pingpong.emit("ping");
-  };
-})();
+error();
